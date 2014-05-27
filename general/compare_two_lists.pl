5e37a77c1bf8bc9dccd0613185a2adf7b65566ba
bio-boris <bsadkhin@gmail.com>
2014-05-27 13:34:12 -0500

    Update compare_two_lists.pl

--- general/compare_two_lists.pl
+++ general/compare_two_lists.pl
@@ -5,16 +5,17 @@
 # Summary: Use this tool to subtract items items from a todo list and see what is left on the todo list  
 use strict;
 
-my $to_process = shift @ARGV;
-my $processed = shift @ARGV;
+my $to_process = $ARGV[0];
+my $processed = $ARGV[1];
 my $usage = "\n --usage compare_two_lists.pl <Set Of Files to Process> <A list of files that have been processed> --return A list of files that still need to be processed \n";
 
 if(scalar @ARGV < 2){
+        print "scalar @ARGV\n";
         die $usage;
 }
 
 open F, $to_process or die $! . $usage . "\n";
-open G, $processed or die $! . $usage . "\n";
+open G, $processed  or die $! . $usage . "\n";
 
 my @to_process = <F>;
 my @processed  = <G>;
